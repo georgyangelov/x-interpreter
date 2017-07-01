2ad90406cb9ea36c040973f92421b934de2510f2
Georgy Angelov <georgyangelov@gmail.com>
2017-07-01 21:50:17 +0300

    Define a method on the ASTClass

--- src/net/gangelov/x/runtime/types/ASTClass.java
+++ src/net/gangelov/x/runtime/types/ASTClass.java
@@ -1,9 +1,21 @@
 package net.gangelov.x.runtime.types;
 
+import net.gangelov.x.ast.ASTNode;
+import net.gangelov.x.runtime.Value;
 import net.gangelov.x.runtime.base.Class;
+import net.gangelov.x.runtime.base.Method;
+import net.gangelov.x.runtime.builtins.StringValue;
+
+import java.util.List;
 
 public class ASTClass extends Class {
     public ASTClass() {
         super("AST");
+
+        defineMethod(new Method("inspect", (List<Value> args) -> {
+            ASTNode self = (ASTNode)args.get(0);
+
+            return new StringValue(self.inspect());
+        }));
     }
 }
