1cdb9226a1598e11187725f6a42f6d35ac4efffd
Georgy Angelov <georgyangelov@gmail.com>
2017-07-02 09:35:03 +0300

    Remove some commented-out lines in the lexer tests

--- test/tests/LexerTest.java
+++ test/tests/LexerTest.java
@@ -41,17 +41,12 @@ class LexerTest {
     @Test
     void testNumbers() throws IOException, Lexer.LexerException {
         assertEquals("(Number '1234') (EOF 'EOF')", lex("1234"));
-//        assertEquals("(Number '-1234') (EOF 'EOF')", lex("-1234"));
-//        assertEquals("(Number '-1234') (EOF 'EOF')", lex("- 1234"));
         assertEquals("(Number '1234.5678') (EOF 'EOF')", lex("1234.5678"));
-//        assertEquals("(Number '-1234.56') (EOF 'EOF')", lex("- 1234.56"));
     }
 
     @Test
     void testCallsOnLiterals() throws IOException, Lexer.LexerException {
-//        assertEquals("(Number '-42') (Dot '.') (Name 'abs') (EOF 'EOF')", lex("- 42.abs"));
         assertEquals("(Number '42.2') (Dot '.') (Name 'abs') (EOF 'EOF')", lex("42.2.abs"));
-//        assertEquals("(Number '-42.2') (Dot '.') (Name 'abs') (EOF 'EOF')", lex("- 42.2.abs"));
         assertEquals("(String 'test') (Dot '.') (Name 'length') (Number '3') (EOF 'EOF')", lex("\"test\".length 3"));
     }
 
